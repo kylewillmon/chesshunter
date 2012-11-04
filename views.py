792f2f4cb83481b583a2e627afd61c9c9619bce3
Kyle Willmon <kylewillmon@gmail.com>
2012-11-03 21:02:53 -0500

    Moved view functions into a view class

--- views.py
+++ views.py
@@ -4,52 +4,56 @@ from pyramid.security import remember, forget, authenticated_userid
 
 from models import Game, User, DBSession
 
-@view_config(route_name="new_game", renderer='json', permission="edit")
-def new_game(request):
-    session = DBSession()
-    game = Game(white="kylewillmon", black="sheerluck")
-    session.add(game)
-    session.commit()
-    return {'status': 'success',
-            'game': game.__json__()}
+class Chesshunter(object):
+    def __init__(self, request):
+        self.request = request
+        self.session = DBSession()
+        self.logged_in = authenticated_userid(request)
 
-@view_config(route_name="view_game", renderer='json', permission="view")
-def view_game(request):
-    session = DBSession()
-    game_id = request.matchdict['game_id']
-    game = session.query(Game).filter(Game.id==game_id).first()
-    if not game:
-        raise HTTPNotFound
-    return {'status': 'success',
-            'game': game.__json__()}
+    @view_config(route_name="new_game", renderer='json', permission="edit")
+    def new_game(self):
+        game = Game(white="kylewillmon", black="sheerluck")
+        self.session.add(game)
+        self.session.commit()
+        return {'status': 'success',
+                'game': game.__json__()}
 
-@view_config(route_name='home', renderer='templates/home.pt')
-def home_view(request):
-    return {}
+    @view_config(route_name="view_game", renderer='json', permission="view")
+    def view_game(self):
+        game_id = self.request.matchdict['game_id']
+        game = self.session.query(Game).filter(Game.id==game_id).first()
+        if not game:
+            raise HTTPNotFound
+        return {'status': 'success',
+                'game': game.__json__()}
 
-@view_config(route_name='login', renderer='templates/login.pt')
-def login(request):
-    session = DBSession()
-    logged_in = authenticated_userid(request)
-    message = 'Please log in'
-    if logged_in:
-        raise HTTPFound(location=request.route_url('home'))
-    if 'submit' in request.POST:
-        username = request.POST.get('username', '')
-        password = request.POST.get('password', '')
-        user = session.query(User).filter(User.username==username).first()
-        if user:
-            if user.check_password(password):
-                headers = remember(request, user.id)
-                raise HTTPFound(location='/', headers=headers)
+    @view_config(route_name='home', renderer='templates/home.pt')
+    def home_view(self):
+        return {}
+
+    @view_config(route_name='login', renderer='templates/login.pt')
+    def login(self):
+        message = 'Please log in'
+        if self.logged_in:
+            url = self.request.route_url('home')
+            raise HTTPFound(location=url)
+        if 'submit' in self.request.POST:
+            username = self.request.POST.get('username', '')
+            password = self.request.POST.get('password', '')
+            user = (self.session.query(User)
+                    .filter(User.username==username).first())
+            if user:
+                if user.check_password(password):
+                    headers = remember(self.request, user.id)
+                    raise HTTPFound(location='/', headers=headers)
+                else:
+                    message = 'Invalid password'
             else:
-                message = 'Invalid password'
-        else:
-            message = 'Invalid user'
-    return {'message': message}
+                message = 'Invalid user'
+        return {'message': message}
 
-@view_config(route_name='logout')
-def login(request):
-        headers = forget(request)
-        loc = request.route_url('home')
-        raise HTTPFound(location=loc, headers=headers)
+    @view_config(route_name='logout')
+    def login(self):
+            headers = forget(self.request)
+            url = self.request.route_url('home')
+            raise HTTPFound(location=url, headers=headers)

1cd78c4faa785eb94016af453a0fd57497f768dd
Kyle Willmon <kylewillmon@gmail.com>
2012-11-18 20:02:03 -0600

    Added database url for heroku

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 import chesshunter
 import logging
+import os
 from wsgiref.simple_server import make_server
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     logging.getLogger('sqlalchemy').setLevel(logging.INFO)
+    db = os.getenv('HEROKU_POSTGRESQL_VIOLET_URL', 'sqlite:///chesshunter.db')
     settings = {
         'auth.secret': 'chsecret',
-        'sqlalchemy.url': 'sqlite:///chesshunter.db',
+        'sqlalchemy.url': db,
         'mako.directories': 'templates',
     }
     app = chesshunter.main({}, **settings)

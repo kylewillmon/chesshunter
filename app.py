750c1fea6b138a9dd3fd7d2fa1fa00853cd85341
Kyle Willmon <kylewillmon@gmail.com>
2012-11-18 20:14:53 -0600

    Bind to the port specified by env variable PORT

--- app.py
+++ app.py
@@ -7,11 +7,12 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     logging.getLogger('sqlalchemy').setLevel(logging.INFO)
     db = os.getenv('HEROKU_POSTGRESQL_VIOLET_URL', 'sqlite:///chesshunter.db')
+    port = os.getenv('PORT', 8080)
     settings = {
         'auth.secret': 'chsecret',
         'sqlalchemy.url': db,
         'mako.directories': 'templates',
     }
     app = chesshunter.main({}, **settings)
-    server = make_server('0.0.0.0', 8080, app)
+    server = make_server('0.0.0.0', port, app)
     server.serve_forever()
